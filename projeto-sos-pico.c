ba8ad614c352964298db8fea0baf41a6eb9ae910
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2025-01-12 00:17:43 -0300

    funcao para enviar um ponto

--- projeto-sos-pico.c
+++ projeto-sos-pico.c
@@ -8,6 +8,14 @@
 #define INTERVALO_LETRA 250 // Intervalo entre letras (ms)
 #define INTERVALO_CICLO 3000 // Intervalo entre ciclos SOS (ms)
 
+// Função para enviar um ponto
+void enviar_ponto() {
+    gpio_put(PINO_LED, true);
+    sleep_ms(TEMPO_PONTO);
+    gpio_put(PINO_LED, false);
+    sleep_ms(INTERVALO_GAP);
+}
+
 int main()
 {
     stdio_init_all();
