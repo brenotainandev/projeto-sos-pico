ff482a1157d8956fa1b5550ffbdaecddc1da2b54
Breno Tainan Aguiar <brenotaguiar@gmail.com>
2025-01-12 00:16:41 -0300

    configuracao de pino, duracao e intervalo

--- projeto-sos-pico.c
+++ projeto-sos-pico.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+#define PINO_LED 13 // Pino conectado ao LED
+#define TEMPO_PONTO 200   // Duração de um ponto (ms)
+#define TEMPO_TRACO 800   // Duração de um traço (ms)
+#define INTERVALO_GAP 125 // Intervalo entre pontos e traços (ms)
+#define INTERVALO_LETRA 250 // Intervalo entre letras (ms)
+#define INTERVALO_CICLO 3000 // Intervalo entre ciclos SOS (ms)
 
 int main()
 {
